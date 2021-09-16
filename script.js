214778cff0c03e94d709efbf1f14f7b320343de0
Jayden Craig <35790878+jcraig3@users.noreply.github.com>
2021-09-16 11:15:26 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const principalBox = document.querySelector("#principal")
+const interestRateBox = document.querySelector("#rate")
+const numberOfCompoundsBox = document.querySelector("#compounded")
+const numberOfYearsBox = document.querySelector("#years")
+const earnedSpan = document.querySelector("#earned")
+const totalSpan = document.querySelector("#total")
+
+function computeInterest() {
+  const principal = principalBox.value
+  const interest = interestRateBox.value
+  const compound = numberOfCompoundsBox.value
+  const years = numberOfYearsBox.value
+
+  var multiplier = (interest / compound) + 1
+  var multiplierPower = multiplier ** years
+  var amount = principal * multiplierPower
+
+  earnedSpan.textContent = amount - principal
+  totalSpan.textContent = amount
+}
+
+principalBox.addEventListener('input', computeInterest)
+interestRateBox.addEventListener('input', computeInterest)
+numberOfCompoundsBox.addEventListener('input', computeInterest)
+numberOfYearsBox.addEventListener('input', computeInterest)
